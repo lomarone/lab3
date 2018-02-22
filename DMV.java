54cc370a6d225d90f7647f561bf31289be9effd0
lomarone <35951241+lomarone@users.noreply.github.com>
2018-02-21 18:31:23 -0800

    Create DMV.java

---
+++ DMV.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.lang.*;
+import java.util.Scanner;
+
+// Driver Program //
+
+public class DMV {
+
+	public static void main (String args[]) {
+
+		String[][] licenses = new String[20][6];
+
+		scan = new Scanner(System.in);
+
+		System.out.println("Welcome to the DMV!");
+
+		System.out.println("Please enter the first, middle, and last name of the applicant: ");
+		String name = scan.nextLine();
+
+		System.out.println("Enter date of birth: ");
+		String birth = scan.nextLine();
+
+		System.out.println("1. Issue a new license.");
+		System.out.println("2. List all the licensed drivers to the screen.");
+		System.out.println("3. List the information of a particular driver if provided a specific license number.");
+		System.out.println("4. Change the name of the driver if provided a specific license number.");
+
+		System.out.println("Choose an option: ");
+		int employee = scan.nextLine();
+
+		if (employee == "1") {
+
+		}
+
+		else if (employee ==  "2") {
+			System.out.print(licenses[0][])
+
+		}
+
+		else if (employee == "3") {
+
+		}
+
+		else {
+
+		}
+
+	}
+
+
+
+	
+
+	
+	
+}
